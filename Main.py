54902fcff7798163744f59d091dfccc2eac5c6ef
Atharv Gangarde <69358946+Atharv-101@users.noreply.github.com>
2024-11-08 12:15:10 +0000

    commit ke 1

--- Main.py
+++ Main.py
@@ -12,6 +12,10 @@ ctr = 1
 now = datetime.datetime.now()
 
 f = open("commit.txt", "w")
+os.system("git config user.name")
+os.system("git config user.email")
+os.system("git init")
+
 pointer = 0
 
 while tl > 0:
@@ -20,12 +24,16 @@ while tl > 0:
         f = open("commit.txt", "a+")
         l_date = now + datetime.timedelta(days=-pointer)
         formatdate = l_date.strftime("%Y-%m-%d")
-        f.write(f"commit to {ctr}: {formatdate}\n")
+        f.write(f"commit ke {ctr}: {formatdate}\n")
         f.close()
         os.system("git add .")
-        os.system(f"git commit --date=\"{formatdate} 12:15:10\" -m \"commit to {ctr}\"")
-        print(f"commit to {ctr}: {formatdate}")
+        os.system(f"git commit --date=\"{formatdate} 12:15:10\" -m \"commit ke {ctr}\"")
+        print(f"commit ke {ctr}: {formatdate}")
         ct-=1
         ctr+=1
     pointer+=1
     tl-=1
+
+os.system(f"git remote add origin {repo_link}")
+os.system("git branch -M main")
+os.system("git push -u origin main -f")
