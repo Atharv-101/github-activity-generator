f7a29873442c83cc147c689f9deecdefc5ab8c5b
Atharv Gangarde <69358946+Atharv-101@users.noreply.github.com>
2024-11-08 18:57:37 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+import os
+from datetime import date, time, datetime
+import datetime
+
+total_day = 40 #total days back
+commit_frequency = 10 #commit time per day
+repo_link = "https://github.com/Atharv-101/git_practice.git"
+
+tl = total_day #time day
+ctr = 1
+
+now = datetime.datetime.now()
+
+f = open("commit.txt", "w")
+pointer = 0
+
+while tl > 0:
+    ct = commit_frequency
+    while ct > 0:
+        f = open("commit.txt", "a+")
+        l_date = now + datetime.timedelta(days=-pointer)
+        formatdate = l_date.strftime("%Y-%m-%d")
+        f.write(f"commit ke {ctr}: {formatdate}\n")
+        f.close()
+        os.system("git add .")
+        os.system(f"git commit --date=\"{formatdate} 12:15:10\" -m \"commit ke {ctr}\"")
+        print(f"commit ke {ctr}: {formatdate}")
+        ct-=1
+        ctr+=1
+    pointer+=1
+    tl-=1
